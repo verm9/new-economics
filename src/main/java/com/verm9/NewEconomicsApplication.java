9bb7b55e88a4f4b6cc17a38db89bfa27773e4728
verm9 <verm9999@gmail.com>
2017-02-20 22:23:51 +0300

    Spring boot init: web, jpa, H2

---
+++ src/main/java/com/verm9/NewEconomicsApplication.java
@@ -0,0 +1,12 @@
+package com.verm9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEconomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEconomicsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/verm9/NewEconomicsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.verm9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewEconomicsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
