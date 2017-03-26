4d52166a5bf1870aa51a98d4583a150484c9a877
verm9 <verm9>
2017-03-26 21:10:10 +0300

    Small fixes

--- src/main/resources/static/js/sendMainPageData.js
+++ src/main/resources/static/js/sendMainPageData.js
@@ -6,11 +6,13 @@ function call() {
         data: msg,
         success: function(data) {
             $('#roiDiv').show();
+            $('#roiParagraph').html("ROI: <b>" + Number(data.roiDays).toFixed(0) + "</b> days (" + Number(365 / data.roiDays * 100).toFixed(0) + "%/year)");
             $('#profitTable').show();
             $('#profitInCryptoCurrencyTd').html(data.profitInCryptoCurrencyTd);
             $('#profitInBtcTd').innerHTML = data.profitInCryptoCurrencyTd;
             $('#profitInUsdTd').text(data.profitInCryptoCurrencyTd.toString());
             $('#profitInRubTd').text(data.toString());
+            $('#wastedPowerInRubTd').html(data);
             console.log(data.profitInCryptoCurrency);
             console.log(data);
         },
