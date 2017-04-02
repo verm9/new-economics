8cb3103832719108d3f83d8046b0ba0be0355e91
eignatik <evg.ignatik@gmail.com>
2017-04-02 14:39:58 +0300

    fix displaying response

--- src/main/resources/static/js/sendMainPageData.js
+++ src/main/resources/static/js/sendMainPageData.js
@@ -5,15 +5,15 @@ function call() {
         url: '/eth/doCalculationsForAMonth',
         data: msg,
         success: function(data) {
+            const localData = data;
             $('#roiDiv').show();
             $('#roiParagraph').html("ROI: <b>" + Number(data.roiDays).toFixed(0) + "</b> days (" + Number(365 / data.roiDays * 100).toFixed(0) + "%/year)");
             $('#profitTable').show();
-            $('#profitInCryptoCurrencyTd').html(data.profitInCryptoCurrencyTd);
-            $('#profitInBtcTd').innerHTML = data.profitInCryptoCurrencyTd;
-            $('#profitInUsdTd').text(data.profitInCryptoCurrencyTd.toString());
-            $('#profitInRubTd').text(data.toString());
-            $('#wastedPowerInRubTd').html(data);
-            console.log(data.profitInCryptoCurrency);
+            $('#profitInCryptoCurrencyTd').html(localData.profitInCryptoCurrency + "");
+            $('#profitInBtcTd').html(localData.profitInBtc + "");
+            $('#profitInUsdTd').html(localData.profitInUsd + "");
+            $('#profitInRubTd').html(localData.profitInRub + "");
+            $('#wastedPowerInRubTd').html(localData.wastedPowerInRub + "");
             console.log(data);
         },
         error:  function(xhr, str){
