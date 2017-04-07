2ff57ef938132c68b31d7f562f0ec29f906c682d
verm9 <verm9>
2017-04-07 08:44:07 +0300

    step fixes, rounding btc price

--- src/main/java/com/verm9/ne/controller/MainControllerImpl.java
+++ src/main/java/com/verm9/ne/controller/MainControllerImpl.java
@@ -2,6 +2,7 @@ package com.verm9.ne.controller;
 
 import com.verm9.ne.repository.model.EthTimepoint;
 import com.verm9.ne.service.EthServiceImpl;
+import org.apache.commons.math3.util.Precision;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,7 @@ public class MainControllerImpl implements MainController {
         EthTimepoint lastTimepoint = Optional.ofNullable(service.getLastTimepoint()).orElse(new EthTimepoint());
 
         model.addAttribute( "netHashRate", Optional.ofNullable(lastTimepoint.getTotalHashRate()).orElse(15087740000000D) );
-        model.addAttribute( "cryptoCurrencyToBtc", Optional.ofNullable(lastTimepoint.getPrice()).orElse(0.0365) );
+        model.addAttribute( "cryptoCurrencyToBtc", Precision.round(Optional.ofNullable(lastTimepoint.getPrice()).orElse(0.0365),    4) );
         return "main";
     }
 
